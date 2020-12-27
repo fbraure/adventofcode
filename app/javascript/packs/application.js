d1618e2eee66ff66486f4f893214fb4235ae76ba
Florent BRAURE <florent.braure@gmail.com>
2020-12-27 22:23:38 +0100

    Add day 20

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,26 +26,27 @@ require("channels")
 import "bootstrap";
 
 // Internal imports
-import { exec_1 } from '../days/1.js';
-import { exec_2 } from '../days/2.js';
-import { exec_3 } from '../days/3.js';
-import { exec_4 } from '../days/4.js';
-import { exec_5 } from '../days/5.js';
-import { exec_6 } from '../days/6.js';
-import { exec_7 } from '../days/7.js';
-import { exec_8 } from '../days/8.js';
-import { exec_9 } from '../days/9.js';
-import { exec_10 } from '../days/10.js';
-import { exec_11 } from '../days/11.js';
-import { exec_12 } from '../days/12.js';
-import { exec_13 } from '../days/13.js';
-import { exec_14 } from '../days/14.js';
-import { exec_14_2 } from '../days/14_2.js';
-import { exec_15 } from '../days/15.js';
-import { exec_16 } from '../days/16.js';
-import { exec_17 } from '../days/17.js';
-import { exec_18 } from '../days/18.js';
-import { exec_19 } from '../days/19.js';
+// import { exec_1 } from '../days/1.js';
+// import { exec_2 } from '../days/2.js';
+// import { exec_3 } from '../days/3.js';
+// import { exec_4 } from '../days/4.js';
+// import { exec_5 } from '../days/5.js';
+// import { exec_6 } from '../days/6.js';
+// import { exec_7 } from '../days/7.js';
+// import { exec_8 } from '../days/8.js';
+// import { exec_9 } from '../days/9.js';
+// import { exec_10 } from '../days/10.js';
+// import { exec_11 } from '../days/11.js';
+// import { exec_12 } from '../days/12.js';
+// import { exec_13 } from '../days/13.js';
+// import { exec_14 } from '../days/14.js';
+// import { exec_14_2 } from '../days/14_2.js';
+// import { exec_15 } from '../days/15.js';
+// import { exec_16 } from '../days/16.js';
+// import { exec_17 } from '../days/17.js';
+// import { exec_18 } from '../days/18.js';
+// import { exec_19 } from '../days/19.js';
+import { exec_20 } from '../days/20.js';
 document.addEventListener('turbolinks:load', () => {
   // exec_1();
   // exec_2();
@@ -66,5 +67,6 @@ document.addEventListener('turbolinks:load', () => {
   // exec_16(); // Chiant
   // exec_17(); // Recheck calcul
   // exec_18();
-  exec_19();
+  // exec_19();
+  exec_20();
 });
